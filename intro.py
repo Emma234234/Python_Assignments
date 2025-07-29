ef470e6ffe84bce06ca24b57f111adce7dffec6f
Emma 234234 <immamumbua9@gmail.com>
2025-07-29 20:16:24 +0300

    Added intro.py

---
+++ intro.py
@@ -0,0 +1,11 @@
+
+languages = ["Python", "Dart", "Web", 23]
+print(languages)
+
+#logical operators
+a = 10
+b = 20
+c = 30
+print (a < b and b < c) #True
+print (a < b or b < c) #True
+print (not(a < b)) #True
